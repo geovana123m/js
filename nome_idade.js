34b13a35d299866431bb5abd62008d9b86c2aa4c
geovana123m <128698692+geovana123m@users.noreply.github.com>
2024-02-23 11:54:54 -0300

    Add files via upload

---
+++ nome_idade.js
@@ -0,0 +1,8 @@
+let nome = `geo`;
+let idade = 17;
+let altura = 170;
+let peso = 70;
+let inteiro = 2;
+
+console.log(`meu nome é ${nome} e minha idade é ${idade} e minha altura é ${altura} e meu peso é ${peso}`)
+console.log(`somando ${idade + inteiro}`)
