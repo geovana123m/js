38c9c7b2f7910094481cc99df00f6374b25114bc
geovana123m <128698692+geovana123m@users.noreply.github.com>
2024-03-04 11:16:06 -0300

    Add files via upload

---
+++ contas.js
@@ -0,0 +1,27 @@
+let soma1 = 5.000 + 5.000;
+let soma2 = 10.000 + 5.000;
+let soma3 = 15.000 + 0.350;
+
+let subtracao1 = -50 - 50;
+let subtracao2 = -100 - 100;
+let subtracao3 = -200 - 200;
+let subtracao4 = -400 - 100;
+let subtracao5 = -500 - 67;
+
+let multiplicacao1 = 490 * 2;
+let multiplicacao2 = 980 * 1;
+
+let divisao1 = 567 / 3;
+let divisao2 = 189 / 3;
+let divisao3 = 63 / 3;
+
+let restoDivisao1 = 3583 % 1896;
+let restoDivisao2 = 1687 % 923;
+let restoDivisao3 = 764 % 560;
+let restoDivisao4 = 204 % 148;
+
+console.log(soma3. toFixed(3))
+console.log(subtracao5)
+console.log(multiplicacao2)
+console.log(divisao3)
+console.log(restoDivisao4)
